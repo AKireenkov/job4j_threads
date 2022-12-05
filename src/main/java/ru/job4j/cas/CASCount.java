3b342481e4baf7b5183407d650f770be19c6604c
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-12-05 15:40:29 +0300

    0. CAS - операции [#6859 #367476]

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -2,30 +2,20 @@ package ru.job4j.cas;
 
 import net.jcip.annotations.ThreadSafe;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
 @ThreadSafe
 public class CASCount {
-    private final AtomicReference<Integer> count = new AtomicReference<>();
-
-    private static AtomicInteger num = new AtomicInteger();
+    private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
     public void increment() {
         int currentNum;
         do {
             currentNum = count.get();
-        } while (!count.compareAndSet(currentNum, num.incrementAndGet()));
+        } while (!count.compareAndSet(currentNum, currentNum + 1));
     }
 
     public int get() {
-        int currentNum;
-        do {
-            if (count.get() == null) {
-                throw new IllegalStateException("count is empty");
-            }
-            currentNum = count.get();
-        } while (!count.compareAndSet(currentNum, num.decrementAndGet()));
-        return currentNum;
+        return count.get();
     }
 }
