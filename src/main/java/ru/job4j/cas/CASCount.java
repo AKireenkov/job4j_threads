e231529211b4e6967cc3f69f520f2734d5a007ed
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-12-06 10:40:08 +0300

    добавлен javadoc

--- src/main/java/ru/job4j/cas/CASCount.java
+++ src/main/java/ru/job4j/cas/CASCount.java
@@ -4,10 +4,23 @@ import net.jcip.annotations.ThreadSafe;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Реализация неблокирующего счетчика по шаблону check-then-act.
+ *
+ * @author Andrey Kireenkov
+ * @version 1.0
+ * @since 06.12.2022
+ */
 @ThreadSafe
 public class CASCount {
     private final AtomicReference<Integer> count = new AtomicReference<>(0);
 
+    /**
+     * Метод увеличивает значение счетчика на 1.
+     * С ним могут работать несколько нитей. Достикается за счет получения текущего значениея счетчика в do
+     * после этого, сравниваем текущее значение с ранее полученным.
+     * Если они совпадают, тогда увеличиваем значение на 1. Если нет - еще раз получаем значение счетчика.
+     */
     public void increment() {
         int currentNum;
         do {
@@ -15,6 +28,9 @@ public class CASCount {
         } while (!count.compareAndSet(currentNum, currentNum + 1));
     }
 
+    /**
+     * @return возврщает текущее значение счетчика.
+     */
     public int get() {
         return count.get();
     }
