8e22830ad76d077736bb06d0e754931c6c810857
Киреенков Андрей <AKireenkov@alfabank.ru>
2022-12-07 21:26:01 +0300

    2. ExecutorService рассылка почты. [#63097 #368057]

--- src/main/java/ru/job4j/email/EmailNotification.java
+++ src/main/java/ru/job4j/email/EmailNotification.java
@@ -4,24 +4,29 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class EmailNotification {
-    ExecutorService pool = Executors.newCachedThreadPool();
+    private final ExecutorService pool = Executors.newFixedThreadPool(
+            Runtime.getRuntime().availableProcessors()
+    );
 
     public void emailTo(User user) {
         String email = user.getEmail();
         String subject = String.format("Notification %s to email %s", user.getUsername(), email);
         String body = String.format("Add a new event to %s", email);
 
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                send(subject, body, email);
-            }
-        });
+
+        pool.submit(() -> send(subject, body, email));
         close();
     }
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ie) {
+                ie.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
