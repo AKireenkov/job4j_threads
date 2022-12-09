17da1b94456f10cb9405c0b4ecad3bf3b1ec9869
andreykireenkov <kireeshka32@gmail.com>
2022-12-09 20:58:59 +0300

    добавлен javadoc

--- src/main/java/ru/job4j/email/User.java
+++ src/main/java/ru/job4j/email/User.java
@@ -1,5 +1,13 @@
 package ru.job4j.email;
 
+/**
+ * Модель данных User.
+ * Содержит поля с информацией о пользователе: username, email
+ *
+ * @author Andrey Kireenkov
+ * @version 1.0
+ * @since 09.12.2022
+ */
 public class User {
     private final String username;
     private final String email;
